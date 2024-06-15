40f0660c3125423829bdd46d3f21642d0cb5b947
Mayank Chudasama <164185463+CyberBoy-Mayank@users.noreply.github.com>
2024-06-15 11:06:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+import FaceDetectionModule as fmm
 import cv2
-import FaceDetectionModule as fdm
 from time import time
 
 
 def main():
     cap = cv2.VideoCapture(0)
     pTime = 0
-    detector = fdm.FaceDetector()
+    detector = fmm.FaceMeshDetector(maxFaces=1)
     while True:
         success, img = cap.read()
-        img, bboxs = detector.findFaces(img)
+        img, faces = detector.findFaceMesh(img)
         cTime = time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
         cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-        cv2.imshow("Face Detection", img)
+        cv2.imshow("Face Mesh", img)
         if cv2.waitKey(1) == 27:
             break
     cap.release()
